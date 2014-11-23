ed5ddaaf0f6a12af11c0390a442edfd097d9799c
spijs <thijsdieltjens@gmail.com>
2014-11-23 17:18:05 +0100

    wijzigingen waren weg...

--- sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/polymorphicviews/plugin/PolymorphicChartParameters.java
+++ sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/polymorphicviews/plugin/PolymorphicChartParameters.java
@@ -1,7 +1,5 @@
 package be.kuleuven.cs.oss.polymorphicviews.plugin;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.sonar.api.charts.ChartParameters;
 
@@ -16,8 +14,6 @@ public class PolymorphicChartParameters {
 	public static final String PARAM_BOXWIDTH = "boxwidth";
 	public static final String PARAM_BOXHEIGHT = "boxheight";
 	public static final String PARAM_BOXCOLOR = "boxcolor";
-	public static final String PARAM_LOCALE = "locale";
-
 	public static final String DEFAULT_RESOURCES = "classes";
 	public static final String DEFAULT_TYPE = "scatter"; 
 	public static final String DEFAULT_BOXWIDTH = "13";
@@ -27,8 +23,7 @@ public class PolymorphicChartParameters {
 	public static String DEFAULT_PARENT;
 	public static String DEFAULT_XMETRIC;
 	public static String DEFAULT_YMETRIC;
-	private Map<String, String> params = new HashMap<String, String>();
-	private static final String[] properties = {"resources","parent","type","xmetric","ymetric","size","boxwidth","boxheight","boxcolor"};
+	private ChartParameters chartParameters;
 
 	
 	/**
@@ -36,16 +31,14 @@ public class PolymorphicChartParameters {
 	 * @param p
 	 */
 	public PolymorphicChartParameters(ChartParameters p) {
-		for(String s : properties) {
-			this.params.put(s,p.getValue(s));
-		}
+		this.chartParameters = p;
 	}
 
 	/**
 	 * @return resources
 	 */
 	public String getResources() {
-		String result = getValue(PARAM_RESOURCES,DEFAULT_RESOURCES);
+		String result = chartParameters.getValue(PARAM_RESOURCES,DEFAULT_RESOURCES,false);
 		return result;
 	}
 
@@ -53,7 +46,7 @@ public class PolymorphicChartParameters {
 	 * @return parent
 	 */
 	public String getParent(){
-		String result = getValue(PARAM_PARENT, DEFAULT_PARENT);
+		String result = chartParameters.getValue(PARAM_PARENT, DEFAULT_PARENT,false);
 		return result;
 	}
 	
@@ -61,7 +54,7 @@ public class PolymorphicChartParameters {
 	 * @return xmetric
 	 */
 	public String getXMetric(){
-		String result = getValue(PARAM_XMETRIC, DEFAULT_XMETRIC);
+		String result = chartParameters.getValue(PARAM_XMETRIC, DEFAULT_XMETRIC,false);
 		return result;
 	}
 	
@@ -69,49 +62,33 @@ public class PolymorphicChartParameters {
 	 * @return ymetric
 	 */
 	public String getYMetric(){
-		String result = getValue(PARAM_YMETRIC, DEFAULT_YMETRIC);
+		String result = chartParameters.getValue(PARAM_YMETRIC, DEFAULT_YMETRIC,false);
 		return result;
 	}
 	
 	public String getType(){
-		String result = getValue(PARAM_TYPE, DEFAULT_TYPE);
+		String result = chartParameters.getValue(PARAM_TYPE, DEFAULT_TYPE,false);
 		return result;
 	}
 
 	public String getBoxWidth(){
-		String result = getValue(PARAM_BOXWIDTH, DEFAULT_BOXWIDTH);
+		String result = chartParameters.getValue(PARAM_BOXWIDTH, DEFAULT_BOXWIDTH,false);
 		return result;
 	}
 	
 	public String getSize(){
-		String result = getValue(PARAM_SIZE, DEFAULT_SIZE);
+		String result = chartParameters.getValue(PARAM_SIZE, DEFAULT_SIZE,false);
 		return result;
 	}
 	
 	public String getBoxHeight(){
-		String result = getValue(PARAM_BOXHEIGHT, DEFAULT_BOXHEIGHT);
+		String result = chartParameters.getValue(PARAM_BOXHEIGHT, DEFAULT_BOXHEIGHT,false);
 		return result;
 	}
 	
 	public String getBoxColor(){
-		String result = getValue(PARAM_BOXCOLOR, DEFAULT_BOXCOLOR);
+		String result = chartParameters.getValue(PARAM_BOXCOLOR, DEFAULT_BOXCOLOR,false);
 		return result;
 	}
 
-	  /**
-	   * Returns the [decoded or not] value of a param from its key or the default value
-	   * if id does not exist
-	   *
-	   * @param key the param ket
-	   * @param defaultValue the default value if not exist
-	   * @param decode whther the value should be decoded
-	   * @return the value of the param
-	   */
-	  public String getValue(String key, String defaultValue) {
-		String val = params.get(key);
-	    if (val == null || val.isEmpty()) {
-	      val = defaultValue;
-	    }
-	    return val;
-	  }
 }
