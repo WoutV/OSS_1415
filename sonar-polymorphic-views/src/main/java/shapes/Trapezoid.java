5ee35c25aca777f90ec950933315feac39f17c8d
maximdhollander <maxim.dhollander@student.kuleuven.be>
2014-12-05 20:00:55 +0100

    Basic classes tot nu toe
    nog veel toegevoegd worden

---
+++ sonar-polymorphic-views/src/main/java/shapes/Trapezoid.java
@@ -0,0 +1,13 @@
+package shapes;
+
+import chartbuilder.ChartBuilder;
+
+public class Trapezoid extends Shape{
+
+	@Override
+	public void draw(ChartBuilder builder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
